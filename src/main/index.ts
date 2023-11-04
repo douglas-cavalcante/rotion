a85a200f226d74eb88d6507fc8f451d2a21b8559
Henrique Douglas <henriquedouglas2803@gmail.com>
2023-11-04 15:15:14 -0300

    adiciona tailwind ao projeto

--- src/main/index.ts
+++ src/main/index.ts
@@ -14,11 +14,16 @@ function createWindow(): void {
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
-    }
+    },
+  
   })
+
+ 
+
 
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
+    mainWindow.webContents.openDevTools()
   })
 
   mainWindow.webContents.setWindowOpenHandler((details) => {
--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,9 @@
+import './styles/global.css';
 
 export function App(): JSX.Element {
   return (
     <div className="container">
-      <h1>teste</h1>
+      <h1 className="text-violet-400 font-bold text-4xl">Hello World</h1>
     </div>
   )
 }
